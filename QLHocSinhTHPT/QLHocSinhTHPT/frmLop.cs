d2ba168ba43c9666b582038999ae0eeb058e40fe
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-08-23 22:13:50 +0700

    Update frmLop.cs

--- QLHocSinhTHPT/QLHocSinhTHPT/frmLop.cs
+++ QLHocSinhTHPT/QLHocSinhTHPT/frmLop.cs
@@ -40,6 +40,80 @@ namespace QLHocSinhTHPT
         #endregion
 
 
+        #region BindingNavigatorItems
+
+
+        private void bindingNavigatorExitItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+        private void bindingNavigatorRefreshItem_Click(object sender, EventArgs e)
+        {
+            m_LopCtrl.HienThi(dGVLop, txtMaLop, txtTenLop, cmbKhoiLop, cmbNamHoc, iniSiSo, cmbGiaoVien);
+        }
+
+        public Boolean KiemTraTruocKhiLuu(String cellString)
+        {
+            foreach (DataGridViewRow row in dGVLop.Rows)
+            {
+                if (row.Cells[cellString].Value != null)
+                {
+                    String str = row.Cells[cellString].Value.ToString();
+                    if (str == "")
+                    {
+                        MessageBoxEx.Show("Giá trị của ô không được rỗng!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public Boolean KiemTraSiSoTruocKhiLuu(String siSoColumn)
+        {
+            foreach (DataGridViewRow row in dGVLop.Rows)
+            {
+                if (row.Cells[siSoColumn].Value != null)
+                {
+                    try
+                    {
+                        int siSo = Convert.ToInt32(row.Cells[siSoColumn].Value.ToString());
+
+                        if (quyDinh.KiemTraSiSo(siSo) == false)
+                        {
+                            MessageBoxEx.Show("Sỉ số không đúng quy định!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return false;
+                        }
+                    }
+                    catch
+                    {
+                        MessageBoxEx.Show("Sỉ số phải là một số nguyên!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void bindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            if (KiemTraTruocKhiLuu("colMaLop") == true &&
+                KiemTraTruocKhiLuu("colTenLop") == true &&
+                KiemTraTruocKhiLuu("colMaKhoiLop") == true &&
+                KiemTraTruocKhiLuu("colMaNamHoc") == true &&
+                KiemTraTruocKhiLuu("colMaGiaoVien") == true &&
+                KiemTraSiSoTruocKhiLuu("colSiSo") == true)
+            {
+                m_LopCtrl.LuuLop();
+            }
+        }
+        #endregion
+
+
+
 
     }
 }
