5b6fa55159d9c34ef19fab5376dd6e541b9ab89e
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-08-23 22:13:27 +0700

    Update frmLop.cs

--- QLHocSinhTHPT/QLHocSinhTHPT/frmLop.cs
+++ QLHocSinhTHPT/QLHocSinhTHPT/frmLop.cs
@@ -12,9 +12,34 @@ namespace QLHocSinhTHPT
 {
     public partial class frmLop : Form
     {
+        #region Fields
+        LopCtrl m_LopCtrl = new LopCtrl();
+        NamHocCtrl m_NamHocCtrl = new NamHocCtrl();
+        GiaoVienCtrl m_GiaoVienCtrl = new GiaoVienCtrl();
+        QuyDinh quyDinh = new QuyDinh();
+        #endregion
+
+        #region Constructor
         public frmLop()
         {
             InitializeComponent();
+            DataService.OpenConnection();
         }
+        #endregion
+
+        #region Load
+        private void frmLop_Load(object sender, EventArgs e)
+        {
+            m_NamHocCtrl.HienThiComboBox(cmbNamHoc);
+            m_GiaoVienCtrl.HienThiComboBox(cmbGiaoVien);
+            m_NamHocCtrl.HienThiDataGridViewComboBoxColumn(colMaNamHoc);
+            m_GiaoVienCtrl.HienThiDataGridViewComboBoxColumn(colMaGiaoVien);
+
+            m_LopCtrl.HienThi(dGVLop, txtMaLop, txtTenLop, cmbKhoiLop, cmbNamHoc, iniSiSo, cmbGiaoVien);
+        }
+        #endregion
+
+
+
     }
 }
