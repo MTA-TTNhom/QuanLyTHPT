59e623bfd82ce862c42a053777f0f979273e169a
Hoangthuy125 <thuy94.mta@gmail.com>
2019-08-24 22:43:45 +0700

    thuy

--- QLHocSinhTHPT/QLHocSinhTHPT/frmTimKiemGV.Designer.cs
+++ QLHocSinhTHPT/QLHocSinhTHPT/frmTimKiemGV.Designer.cs
@@ -28,10 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmTimKiemGV";
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTimKiemGV));
+            this.groupBoxDanhSach = new System.Windows.Forms.GroupBox();
+            this.dGVKetQuaTimKiem = new DevComponents.DotNetBar.Controls.DataGridViewX();
+            this.colMaGiaoVien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTenGiaoVien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDiaChi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDienThoai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTenMonHoc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.navPaneLeft = new DevComponents.DotNetBar.NavigationPane();
+            this.navPanelTimKiem = new DevComponents.DotNetBar.NavigationPanePanel();
+            this.cmbCMon = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.cmbTheoCMon = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.col04 = new DevComponents.Editors.ComboItem();
+            this.col05 = new DevComponents.Editors.ComboItem();
+            this.col06 = new DevComponents.Editors.ComboItem();
+            this.cmbTheoDChi = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.col01 = new DevComponents.Editors.ComboItem();
+            this.col02 = new DevComponents.Editors.ComboItem();
+            this.col03 = new DevComponents.Editors.ComboItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDiaChi = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.btnTimKiem = new DevComponents.DotNetBar.ButtonX();
+            this.txtHoTen = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.buttonItemTimKiem = new DevComponents.DotNetBar.ButtonItem();
+            this.groupBoxDanhSach.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVKetQuaTimKiem)).BeginInit();
+            this.navPaneLeft.SuspendLayout();
+            this.navPanelTimKiem.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxDanhSach
+            // 
+            this.groupBoxDanhSach.Controls.Add(this.dGVKetQuaTimKiem);
+            this.groupBoxDanhSach.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxDanhSach.Location = new System.Drawing.Point(247, 0);
+            this.groupBoxDanhSach.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBoxDanhSach.Name = "groupBoxDanhSach";
+            this.groupBoxDanhSach.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBoxDanhSach.Size = new System.Drawing.Size(838, 490);
+            this.groupBoxDanhSach.TabIndex = 2;
+            this.groupBoxDanhSach.TabStop = false;
+            this.groupBoxDanhSach.Text = "Kết quả tìm kiếm";
+
         }
 
         #endregion
