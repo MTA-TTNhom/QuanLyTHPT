3d694d9c826a672ac91f6a51dc7e31f4e7aa9d66
Hoangthuy125 <thuy94.mta@gmail.com>
2019-08-30 22:48:39 +0700

    thuy

--- QLHocSinhTHPT/QLHocSinhTHPT/frmTimKiemGV.Designer.cs
+++ QLHocSinhTHPT/QLHocSinhTHPT/frmTimKiemGV.Designer.cs
@@ -73,7 +73,139 @@
             this.groupBoxDanhSach.TabIndex = 2;
             this.groupBoxDanhSach.TabStop = false;
             this.groupBoxDanhSach.Text = "Kết quả tìm kiếm";
-
+            // 
+            // dGVKetQuaTimKiem
+            // 
+            this.dGVKetQuaTimKiem.AllowUserToAddRows = false;
+            this.dGVKetQuaTimKiem.AllowUserToDeleteRows = false;
+            this.dGVKetQuaTimKiem.AllowUserToResizeColumns = false;
+            this.dGVKetQuaTimKiem.AllowUserToResizeRows = false;
+            this.dGVKetQuaTimKiem.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(243)))), ((int)(((byte)(250)))));
+            this.dGVKetQuaTimKiem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dGVKetQuaTimKiem.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colMaGiaoVien,
+            this.colTenGiaoVien,
+            this.colDiaChi,
+            this.colDienThoai,
+            this.colTenMonHoc});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dGVKetQuaTimKiem.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dGVKetQuaTimKiem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dGVKetQuaTimKiem.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(215)))), ((int)(((byte)(229)))));
+            this.dGVKetQuaTimKiem.Location = new System.Drawing.Point(4, 19);
+            this.dGVKetQuaTimKiem.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dGVKetQuaTimKiem.Name = "dGVKetQuaTimKiem";
+            this.dGVKetQuaTimKiem.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dGVKetQuaTimKiem.Size = new System.Drawing.Size(830, 467);
+            this.dGVKetQuaTimKiem.TabIndex = 4;
+            // 
+            // colMaGiaoVien
+            // 
+            this.colMaGiaoVien.DataPropertyName = "MaGiaoVien";
+            this.colMaGiaoVien.HeaderText = "Mã giáo viên";
+            this.colMaGiaoVien.Name = "colMaGiaoVien";
+            this.colMaGiaoVien.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.colMaGiaoVien.Width = 80;
+            // 
+            // colTenGiaoVien
+            // 
+            this.colTenGiaoVien.DataPropertyName = "TenGiaoVien";
+            this.colTenGiaoVien.HeaderText = "Họ và tên";
+            this.colTenGiaoVien.Name = "colTenGiaoVien";
+            this.colTenGiaoVien.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.colTenGiaoVien.Width = 150;
+            // 
+            // colDiaChi
+            // 
+            this.colDiaChi.DataPropertyName = "DiaChi";
+            this.colDiaChi.HeaderText = "Địa chỉ";
+            this.colDiaChi.Name = "colDiaChi";
+            this.colDiaChi.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.colDiaChi.Width = 150;
+            // 
+            // colDienThoai
+            // 
+            this.colDienThoai.DataPropertyName = "DienThoai";
+            this.colDienThoai.HeaderText = "Điện thoại";
+            this.colDienThoai.Name = "colDienThoai";
+            this.colDienThoai.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // colTenMonHoc
+            // 
+            this.colTenMonHoc.DataPropertyName = "TenMonHoc";
+            this.colTenMonHoc.HeaderText = "Chuyên môn";
+            this.colTenMonHoc.Name = "colTenMonHoc";
+            this.colTenMonHoc.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // navPaneLeft
+            // 
+            this.navPaneLeft.CanCollapse = true;
+            this.navPaneLeft.Controls.Add(this.navPanelTimKiem);
+            this.navPaneLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.navPaneLeft.ItemPaddingBottom = 2;
+            this.navPaneLeft.ItemPaddingTop = 2;
+            this.navPaneLeft.Items.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            this.buttonItemTimKiem});
+            this.navPaneLeft.Location = new System.Drawing.Point(0, 0);
+            this.navPaneLeft.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.navPaneLeft.Name = "navPaneLeft";
+            this.navPaneLeft.NavigationBarHeight = 33;
+            this.navPaneLeft.Padding = new System.Windows.Forms.Padding(1);
+            this.navPaneLeft.Size = new System.Drawing.Size(247, 490);
+            this.navPaneLeft.Style = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.navPaneLeft.TabIndex = 0;
+            // 
+            // 
+            // 
+            this.navPaneLeft.TitlePanel.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.navPaneLeft.TitlePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.navPaneLeft.TitlePanel.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.navPaneLeft.TitlePanel.Location = new System.Drawing.Point(1, 1);
+            this.navPaneLeft.TitlePanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.navPaneLeft.TitlePanel.Name = "panelTitle";
+            this.navPaneLeft.TitlePanel.Size = new System.Drawing.Size(245, 30);
+            this.navPaneLeft.TitlePanel.Style.BackColor1.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.navPaneLeft.TitlePanel.Style.BackColor2.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.navPaneLeft.TitlePanel.Style.Border = DevComponents.DotNetBar.eBorderType.RaisedInner;
+            this.navPaneLeft.TitlePanel.Style.BorderColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.navPaneLeft.TitlePanel.Style.BorderSide = DevComponents.DotNetBar.eBorderSide.Bottom;
+            this.navPaneLeft.TitlePanel.Style.ForeColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+            this.navPaneLeft.TitlePanel.Style.GradientAngle = 90;
+            this.navPaneLeft.TitlePanel.Style.MarginLeft = 4;
+            this.navPaneLeft.TitlePanel.TabIndex = 0;
+            this.navPaneLeft.TitlePanel.Text = "Tìm kiếm giáo viên";
+            // 
+            // navPanelTimKiem
+            // 
+            this.navPanelTimKiem.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.navPanelTimKiem.Controls.Add(this.cmbCMon);
+            this.navPanelTimKiem.Controls.Add(this.cmbTheoCMon);
+            this.navPanelTimKiem.Controls.Add(this.cmbTheoDChi);
+            this.navPanelTimKiem.Controls.Add(this.label3);
+            this.navPanelTimKiem.Controls.Add(this.label2);
+            this.navPanelTimKiem.Controls.Add(this.label1);
+            this.navPanelTimKiem.Controls.Add(this.txtDiaChi);
+            this.navPanelTimKiem.Controls.Add(this.btnTimKiem);
+            this.navPanelTimKiem.Controls.Add(this.txtHoTen);
+            this.navPanelTimKiem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.navPanelTimKiem.Location = new System.Drawing.Point(1, 31);
+            this.navPanelTimKiem.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.navPanelTimKiem.Name = "navPanelTimKiem";
+            this.navPanelTimKiem.ParentItem = this.buttonItemTimKiem;
+            this.navPanelTimKiem.Size = new System.Drawing.Size(245, 425);
+            this.navPanelTimKiem.Style.Alignment = System.Drawing.StringAlignment.Center;
+            this.navPanelTimKiem.Style.BackColor1.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.BarBackground;
+            this.navPanelTimKiem.Style.BackColor2.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.BarBackground2;
+            this.navPanelTimKiem.Style.BorderColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.navPanelTimKiem.Style.ForeColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.ItemText;
+            this.navPanelTimKiem.Style.GradientAngle = 90;
+            this.navPanelTimKiem.TabIndex = 1;
         }
 
         #endregion
