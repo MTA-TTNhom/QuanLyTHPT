f4487db1db9b265bcfff396bf5f6322e2dc29472
BuiThanhHoang <54828059+BuiThanhHoang@users.noreply.github.com>
2019-08-31 22:41:34 +0700

    Update CalendarColumn.cs

--- QLHocSinhTHPT/QLHocSinhTHPT/Component/CalendarColumn.cs
+++ QLHocSinhTHPT/QLHocSinhTHPT/Component/CalendarColumn.cs
@@ -27,8 +27,67 @@ namespace QLHocSinhTHPT.Component
                 base.CellTemplate = value;
             }
         }
-
     }
     #endregion
 
-}
+    #region CalendarCell
+    public class CalendarCell : DataGridViewTextBoxCell
+    {
+        public CalendarCell() : base()
+        {
+            this.Style.Format = "dd/MM/yyyy";
+        }
+
+        public override void InitializeEditingControl(int rowIndex, object initialFormattedValue, DataGridViewCellStyle dataGridViewCellStyle)
+        {
+            base.InitializeEditingControl(rowIndex, initialFormattedValue, dataGridViewCellStyle);
+            CalendarEditingControl ctl = DataGridView.EditingControl as CalendarEditingControl;
+            ctl.Value = (DateTime)this.Value;
+        }
+
+        public override Type EditType
+        {
+            get { return typeof(CalendarEditingControl); }
+        }
+
+        public override Type ValueType
+        {
+            get { return typeof(DateTime); }
+        }
+
+        public override object DefaultNewRowValue
+        {
+            get { return DateTime.Now; }
+        }
+    }
+    #endregion
+
+    #region CalendarEditingControl
+    public class CalendarEditingControl : DateTimePicker, IDataGridViewEditingControl
+    {
+        DataGridView dataGridView;
+        private bool valueChanged = false;
+        int rowIndex;
+
+        public CalendarEditingControl()
+        {
+            this.Format = DateTimePickerFormat.Short;
+        }
+
+        public object EditingControlFormattedValue
+        {
+            get
+            {
+                return this.Value.ToShortDateString();
+            }
+            set
+            {
+                if (value is String)
+                {
+                    this.Value = DateTime.Parse((String)value);
+                }
+            }
+        }
+
+
+    }
