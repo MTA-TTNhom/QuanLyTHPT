946f8195f576c5e0b2f59d4e62245f0c3fade78e
Hoangthuy125 <thuy94.mta@gmail.com>
2019-09-04 18:20:39 +0700

    update

---
+++ QLHocSinhTHPT/QLHocSinhTHPT/frmDangNhap.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace QLHocSinhTHPT
+{
+    partial class frmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+
+            this.gPanelDangNhap = new DevComponents.DotNetBar.Controls.GroupPanel();
+            this.lblPassError = new System.Windows.Forms.Label();
+            this.lblUserError = new System.Windows.Forms.Label();
+            this.btnHuyBo = new DevComponents.DotNetBar.ButtonX();
+            this.btnDangNhap = new DevComponents.DotNetBar.ButtonX();
+            this.lblPassword = new DevComponents.DotNetBar.LabelX();
+            this.lblUsername = new DevComponents.DotNetBar.LabelX();
+            this.txtPassword = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtUsername = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.lblTitle = new DevComponents.DotNetBar.LabelX();
+            this.gPanelDangNhap.SuspendLayout();
+            this.panelTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gPanelDangNhap
+            // 
+            this.gPanelDangNhap.CanvasColor = System.Drawing.SystemColors.Control;
+            this.gPanelDangNhap.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.gPanelDangNhap.Controls.Add(this.lblPassError);
+            this.gPanelDangNhap.Controls.Add(this.lblUserError);
+            this.gPanelDangNhap.Controls.Add(this.btnHuyBo);
+            this.gPanelDangNhap.Controls.Add(this.btnDangNhap);
+            this.gPanelDangNhap.Controls.Add(this.lblPassword);
+            this.gPanelDangNhap.Controls.Add(this.lblUsername);
+            this.gPanelDangNhap.Controls.Add(this.txtPassword);
+            this.gPanelDangNhap.Controls.Add(this.txtUsername);
+            this.gPanelDangNhap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gPanelDangNhap.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gPanelDangNhap.Location = new System.Drawing.Point(0, 49);
+            this.gPanelDangNhap.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gPanelDangNhap.Name = "gPanelDangNhap";
+            this.gPanelDangNhap.Size = new System.Drawing.Size(459, 182);
+            // 
+            // 
+            // 
+            this.gPanelDangNhap.Style.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.gPanelDangNhap.Style.BackColorGradientAngle = 90;
+            this.gPanelDangNhap.Style.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.gPanelDangNhap.Style.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.gPanelDangNhap.Style.BorderBottomWidth = 1;
+            this.gPanelDangNhap.Style.BorderColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.gPanelDangNhap.Style.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.gPanelDangNhap.Style.BorderLeftWidth = 1;
+            this.gPanelDangNhap.Style.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.gPanelDangNhap.Style.BorderRightWidth = 1;
+            this.gPanelDangNhap.Style.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.gPanelDangNhap.Style.BorderTopWidth = 1;
+            this.gPanelDangNhap.Style.CornerDiameter = 4;
+            this.gPanelDangNhap.Style.CornerType = DevComponents.DotNetBar.eCornerType.Rounded;
+            this.gPanelDangNhap.Style.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center;
+            this.gPanelDangNhap.Style.TextColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+            this.gPanelDangNhap.Style.TextLineAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Near;
+            this.gPanelDangNhap.TabIndex = 0;
+            this.gPanelDangNhap.Text = "Thông tin đăng nhập";
+            // 
+            // lblPassError
+            // 
+            this.lblPassError.AutoSize = true;
+            this.lblPassError.BackColor = System.Drawing.Color.Transparent;
+            this.lblPassError.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassError.ForeColor = System.Drawing.Color.Red;
+            this.lblPassError.Location = new System.Drawing.Point(223, 98);
+            this.lblPassError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPassError.Name = "lblPassError";
+            this.lblPassError.Size = new System.Drawing.Size(0, 17);
+            this.lblPassError.TabIndex = 5;
+            // 
+            // lblUserError
+            // 
+            this.lblUserError.AutoSize = true;
+            this.lblUserError.BackColor = System.Drawing.Color.Transparent;
+            this.lblUserError.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserError.ForeColor = System.Drawing.Color.Red;
+            this.lblUserError.Location = new System.Drawing.Point(223, 49);
+            this.lblUserError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUserError.Name = "lblUserError";
+            this.lblUserError.Size = new System.Drawing.Size(0, 17);
+            this.lblUserError.TabIndex = 5;
+        }
+
+        #endregion
+    }
+}
