9962acd7d311d82c2cee04ba62658616ed819a57
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-09-05 21:05:10 +0700

    Update frmHocSinh.cs

--- QLHocSinhTHPT/QLHocSinhTHPT/frmHocSinh.cs
+++ QLHocSinhTHPT/QLHocSinhTHPT/frmHocSinh.cs
@@ -45,5 +45,74 @@ namespace QLHocSinhTHPT
             m_HocSinhCtrl.HienThi(dGVHocSinh, bindingNavigatorHocSinh, txtMaHocSinh, txtTenHocSinh, txtGioiTinh, ckbGTinhNam, ckbGTinhNu, dtpNgaySinh, txtNoiSinh, cmbDanToc, cmbTonGiao, txtHoTenCha, cmbNgheNghiepCha, txtHoTenMe, cmbNgheNghiepMe);
         }
         #endregion
+
+        #region BindingNavigatorItems
+
+        private void bindingNavigatorExitItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+        private void bindingNavigatorRefreshItem_Click(object sender, EventArgs e)
+        {
+            m_HocSinhCtrl.HienThi(dGVHocSinh, bindingNavigatorHocSinh, txtMaHocSinh, txtTenHocSinh, txtGioiTinh, ckbGTinhNam, ckbGTinhNu, dtpNgaySinh, txtNoiSinh, cmbDanToc, cmbTonGiao, txtHoTenCha, cmbNgheNghiepCha, txtHoTenMe, cmbNgheNghiepMe);
+        }
+
+        public Boolean KiemTraTruocKhiLuu(String cellString)
+        {
+            foreach (DataGridViewRow row in dGVHocSinh.Rows)
+            {
+                if (row.Cells[cellString].Value != null)
+                {
+                    String str = row.Cells[cellString].Value.ToString();
+                    if (str == "")
+                    {
+                        MessageBoxEx.Show("Thông tin học sinh " + row.Cells["colHoTen"].Value.ToString() + " không hợp lệ!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public Boolean KiemTraDoTuoiTruocKhiLuu(String doTuoiColumn)
+        {
+            foreach (DataGridViewRow row in dGVHocSinh.Rows)
+            {
+                if (row.Cells[doTuoiColumn].Value != null)
+                {
+                    DateTime ngaySinh = Convert.ToDateTime(row.Cells[doTuoiColumn].Value.ToString());
+
+                    if (quyDinh.KiemTraDoTuoi(ngaySinh) == false)
+                    {
+                        MessageBoxEx.Show("Tuổi học sinh " + row.Cells["colHoTen"].Value.ToString() + " không đúng quy định!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void bindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            if (KiemTraTruocKhiLuu("colMaHocSinh") == true &&
+                KiemTraTruocKhiLuu("colHoTen") == true &&
+                KiemTraTruocKhiLuu("colNoiSinh") == true &&
+                KiemTraTruocKhiLuu("colMaDanToc") == true &&
+                KiemTraTruocKhiLuu("colMaTonGiao") == true &&
+                KiemTraTruocKhiLuu("colHoTenCha") == true &&
+                KiemTraTruocKhiLuu("colMaNNghiepCha") == true &&
+                KiemTraTruocKhiLuu("colHoTenMe") == true &&
+                KiemTraTruocKhiLuu("colMaNNghiepMe") == true)
+            {
+                if (KiemTraDoTuoiTruocKhiLuu("colNgaySinh") == true)
+                {
+                    m_HocSinhCtrl.LuuHocSinh();
+                }
+            }
+        }
+        #endregion
     }
 }
