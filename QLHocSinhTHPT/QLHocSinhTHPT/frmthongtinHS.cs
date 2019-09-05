d1c1adc37c02d2c24bc7260cf79b45c60132f9c0
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-09-05 16:28:08 +0700

    create frmthongtinHS

---
+++ QLHocSinhTHPT/QLHocSinhTHPT/frmthongtinHS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QLHocSinhTHPT
+{
+    public partial class frmthongtinHS : Form
+    {
+        public frmthongtinHS()
+        {
+            InitializeComponent();
+        }
+    }
+}
