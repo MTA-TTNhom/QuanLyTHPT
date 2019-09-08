2763c3fdaa13ed00a4c6a795eaacd39c1a83c8cb
tronglong1998 <54692940+tronglong1998@users.noreply.github.com>
2019-09-09 01:05:12 +0700

    Update lai

---
+++ QLHocSinhTHPT/QLHocSinhTHPT/Bussiness/PhanCongInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//Hoàn chỉnh
+namespace QLHocSinhTHPT.Bussiness
+{
+    public class PhanCongInfo
+    {
+        public PhanCongInfo()
+        {
+
+        }
+
+        private NamHocInfo m_NamHoc;
+        public NamHocInfo NamHoc
+        {
+            get { return m_NamHoc; }
+            set { m_NamHoc = value; }
+        }
+
+       
+    }
+}
---
+++ QLHocSinhTHPT/QLHocSinhTHPT/Bussiness/TonGiaoInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLHocSinhTHPT.Bussiness
+{
+    class TonGiaoInfo
+    {
+    }
+}
