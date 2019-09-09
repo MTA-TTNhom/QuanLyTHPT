f8bc3de196e40824df7bdfc4e1822b88f3a57553
HuynhLeHoang <chunickdeptrailam@gmail.com>
2019-09-10 00:08:29 +0700

    Update LoaiDiemInfo.cs

--- QLHocSinhTHPT/QLHocSinhTHPT/Bussiness/LoaiDiemInfo.cs
+++ QLHocSinhTHPT/QLHocSinhTHPT/Bussiness/LoaiDiemInfo.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
+//Hoàn chỉnh
 namespace QLHocSinhTHPT.Bussiness
 {
-    class LoaiDiemInfo
+    public class LoaiDiemInfo
     {
+        public LoaiDiemInfo()
+        {
+
+        }
+
+        private String m_MaLoai;
+        public String MaLoai
+        {
+            get { return m_MaLoai; }
+            set { m_MaLoai = value; }
+        }
+
+        private String m_TenLoai;
+        public String TenLoai
+        {
+            get { return m_TenLoai; }
+            set { m_TenLoai = value; }
+        }
+
+        private int m_HeSo;
+        public int HeSo
+        {
+            get { return m_HeSo; }
+            set { m_HeSo = value; }
+        }
     }
 }
