5fd2cfb514ae8b493b995e698dae776968e4ea16
HuynhLeHoang <chunickdeptrailam@gmail.com>
2019-09-09 23:59:12 +0700

    Update KhoiLopInfo.cs

--- QLHocSinhTHPT/QLHocSinhTHPT/Bussiness/KhoiLopInfo.cs
+++ QLHocSinhTHPT/QLHocSinhTHPT/Bussiness/KhoiLopInfo.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
+//Hoàn chỉnh
 namespace QLHocSinhTHPT.Bussiness
 {
-    class KhoiLopInfo
+    public class KhoiLopInfo
     {
+        public KhoiLopInfo()
+        {
+
+        }
+
+        private String m_MaKhoiLop;
+        public String MaKhoiLop
+        {
+            get { return m_MaKhoiLop; }
+            set { m_MaKhoiLop = value; }
+        }
+
+        private String m_TenKhoiLop;
+        public String TenKhoiLop
+        {
+            get { return m_TenKhoiLop; }
+            set { m_TenKhoiLop = value; }
+        }
     }
 }
