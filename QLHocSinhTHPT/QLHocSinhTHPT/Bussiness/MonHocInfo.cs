918bdcecb8a47e3ec9ac1ccd62330ed10a99b635
HuynhLeHoang <chunickdeptrailam@gmail.com>
2019-09-10 00:10:29 +0700

    Update MonHocInfo.cs

--- QLHocSinhTHPT/QLHocSinhTHPT/Bussiness/MonHocInfo.cs
+++ QLHocSinhTHPT/QLHocSinhTHPT/Bussiness/MonHocInfo.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
+//Hoàn chỉnh
 namespace QLHocSinhTHPT.Bussiness
 {
-    class MonHocInfo
+    public class MonHocInfo
     {
+        public MonHocInfo()
+        {
+
+        }
+
+        private String m_MaMonHoc;
+        public String MaMonHoc
+        {
+            get { return m_MaMonHoc; }
+            set { m_MaMonHoc = value; }
+        }
+
+        private String m_TenMonHoc;
+        public String TenMonHoc
+        {
+            get { return m_TenMonHoc; }
+            set { m_TenMonHoc = value; }
+        }
+
+        private int m_SoTiet;
+        public int SoTiet
+        {
+            get { return m_SoTiet; }
+            set { m_SoTiet = value; }
+        }
+
+        private int m_HeSo;
+        public int HeSo
+        {
+            get { return m_HeSo; }
+            set { m_HeSo = value; }
+        }
     }
 }
