0c70dee4570ccb189009c7b4aaf4e96408e3e07e
HuynhLeHoang <chunickdeptrailam@gmail.com>
2019-09-10 00:11:13 +0700

    Update NamHocInfo.cs

--- QLHocSinhTHPT/QLHocSinhTHPT/Bussiness/NamHocInfo.cs
+++ QLHocSinhTHPT/QLHocSinhTHPT/Bussiness/NamHocInfo.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
+//Hoàn chỉnh
 namespace QLHocSinhTHPT.Bussiness
 {
-    class NamHocInfo
+    public class NamHocInfo
     {
+        public NamHocInfo()
+        {
+
+        }
+
+        private String m_MaNamHoc;
+        public String MaNamHoc
+        {
+            get { return m_MaNamHoc; }
+            set { m_MaNamHoc = value; }
+        }
+
+        private String m_TenNamHoc;
+        public String TenNamHoc
+        {
+            get { return m_TenNamHoc; }
+            set { m_TenNamHoc = value; }
+        }
     }
 }
