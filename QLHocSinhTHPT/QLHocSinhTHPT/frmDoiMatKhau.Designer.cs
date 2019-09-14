388fec72486439684e838fd3bc1b16c5afa53912
Hoangthuy125 <thuy94.mta@gmail.com>
2019-09-14 20:43:39 +0700

    update

---
+++ QLHocSinhTHPT/QLHocSinhTHPT/frmDoiMatKhau.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace QLHocSinhTHPT
+{
+    partial class frmDoiMatKhau
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gPanelDoiMK = new DevComponents.DotNetBar.Controls.GroupPanel();
+            this.lblReNPassError = new System.Windows.Forms.Label();
+            this.lblNewPassError = new System.Windows.Forms.Label();
+            this.lblOldPassError = new System.Windows.Forms.Label();
+            this.btnHuyBo = new DevComponents.DotNetBar.ButtonX();
+            this.btnDongY = new DevComponents.DotNetBar.ButtonX();
+            this.lblReNewPass = new DevComponents.DotNetBar.LabelX();
+            this.lblNewPass = new DevComponents.DotNetBar.LabelX();
+            this.lblOldPass = new DevComponents.DotNetBar.LabelX();
+            this.txtReNPassword = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtNewPassword = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtOldPassword = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.gPanelDoiMK.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gPanelDoiMK
+            // 
+            this.gPanelDoiMK.CanvasColor = System.Drawing.SystemColors.Control;
+            this.gPanelDoiMK.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.gPanelDoiMK.Controls.Add(this.lblReNPassError);
+            this.gPanelDoiMK.Controls.Add(this.lblNewPassError);
+            this.gPanelDoiMK.Controls.Add(this.lblOldPassError);
+            this.gPanelDoiMK.Controls.Add(this.btnHuyBo);
+            this.gPanelDoiMK.Controls.Add(this.btnDongY);
+            this.gPanelDoiMK.Controls.Add(this.lblReNewPass);
+            this.gPanelDoiMK.Controls.Add(this.lblNewPass);
+            this.gPanelDoiMK.Controls.Add(this.lblOldPass);
+            this.gPanelDoiMK.Controls.Add(this.txtReNPassword);
+            this.gPanelDoiMK.Controls.Add(this.txtNewPassword);
+            this.gPanelDoiMK.Controls.Add(this.txtOldPassword);
+            this.gPanelDoiMK.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gPanelDoiMK.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gPanelDoiMK.Location = new System.Drawing.Point(0, 0);
+            this.gPanelDoiMK.Name = "gPanelDoiMK";
+            this.gPanelDoiMK.Size = new System.Drawing.Size(344, 188);
+            // 
+            // 
+            // 
+            this.gPanelDoiMK.Style.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.gPanelDoiMK.Style.BackColorGradientAngle = 90;
+            this.gPanelDoiMK.Style.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.gPanelDoiMK.Style.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.gPanelDoiMK.Style.BorderBottomWidth = 1;
+            this.gPanelDoiMK.Style.BorderColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.gPanelDoiMK.Style.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.gPanelDoiMK.Style.BorderLeftWidth = 1;
+            this.gPanelDoiMK.Style.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.gPanelDoiMK.Style.BorderRightWidth = 1;
+            this.gPanelDoiMK.Style.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.gPanelDoiMK.Style.BorderTopWidth = 1;
+            this.gPanelDoiMK.Style.CornerDiameter = 4;
+            this.gPanelDoiMK.Style.CornerType = DevComponents.DotNetBar.eCornerType.Rounded;
+            this.gPanelDoiMK.Style.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center;
+            this.gPanelDoiMK.Style.TextColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+            this.gPanelDoiMK.Style.TextLineAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Near;
+            this.gPanelDoiMK.TabIndex = 0;
+            this.gPanelDoiMK.Text = "Đổi mật khẩu cá nhân";
+            // 
+            // lblReNPassError
+            // 
+            this.lblReNPassError.AutoSize = true;
+            this.lblReNPassError.BackColor = System.Drawing.Color.Transparent;
+            this.lblReNPassError.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReNPassError.ForeColor = System.Drawing.Color.Red;
+            this.lblReNPassError.Location = new System.Drawing.Point(167, 120);
+            this.lblReNPassError.Name = "lblReNPassError";
+            this.lblReNPassError.Size = new System.Drawing.Size(0, 13);
+            this.lblReNPassError.TabIndex = 0;
+        }
+
+        #endregion
+    }
+}
