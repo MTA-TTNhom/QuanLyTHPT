a31dc2ea267418b3c2ff090791200fb53a0d1c4b
BuiThanhHoang <54828059+BuiThanhHoang@users.noreply.github.com>
2019-09-14 19:57:42 +0700

    update

--- QLHocSinhTHPT/QLHocSinhTHPT/Component/Utilities.cs
+++ QLHocSinhTHPT/QLHocSinhTHPT/Component/Utilities.cs
@@ -457,4 +457,46 @@ namespace QLHocSinhTHPT.Component
         }
 
     }
+
+    public static void XMLWriter(String filename, String servname, String database, String costatus)
+    {
+        XmlTextWriter xmlW = new XmlTextWriter(filename, null);
+        xmlW.Formatting = Formatting.Indented;
+
+        xmlW.WriteStartDocument();
+        xmlW.WriteComment("\nKhong duoc thay doi noi dung file nay!\n" +
+                            "Thong so co ban:\n\t" +
+                            "costatus = true : quyen Windows\n\t" +
+                            "costatus = false: quyen SQL Server\n\t" +
+                            "servname: ten server\n\t" +
+                            "username: ten dang nhap he thong\n\t" +
+                            "password: mat khau dang nhap he thong\n\t" +
+                            "database: ten co so du lieu\n");
+        xmlW.WriteStartElement("config");
+
+        xmlW.WriteStartElement("costatus");
+        xmlW.WriteString(costatus);
+        xmlW.WriteEndElement();
+
+        xmlW.WriteStartElement("servname");
+        xmlW.WriteString(servname);
+        xmlW.WriteEndElement();
+
+        xmlW.WriteStartElement("username");
+        xmlW.WriteString("");
+        xmlW.WriteEndElement();
+
+        xmlW.WriteStartElement("password");
+        xmlW.WriteString("");
+        xmlW.WriteEndElement();
+
+        xmlW.WriteStartElement("database");
+        xmlW.WriteString(database);
+        xmlW.WriteEndElement();
+
+        xmlW.WriteEndElement();
+        xmlW.WriteEndDocument();
+
+        xmlW.Close();
+    }
 }
