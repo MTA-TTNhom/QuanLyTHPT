e948753c4cbdf40063e34a5e21a6cdf6d227baf9
tronglong1998 <54692940+tronglong1998@users.noreply.github.com>
2019-09-15 20:44:31 +0700

    Create TonGiaoCtrl.cs

---
+++ QLHocSinhTHPT/QLHocSinhTHPT/Controller/TonGiaoCtrl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLHocSinhTHPT.Controller
+{
+    class TonGiaoCtrl
+    {
+
+
+        public class TonGiaoCtrl
+        {
+            TonGiaoData m_TonGiaoData = new TonGiaoData();
+
+            #region Hien thi ComboBox
+            public void HienThiComboBox(ComboBoxEx comboBox)
+            {
+                comboBox.DataSource = m_TonGiaoData.LayDsTonGiao();
+                comboBox.DisplayMember = "TenTonGiao";
+                comboBox.ValueMember = "MaTonGiao";
+            }
+            #endregion
+
+
+        }
+
+
+
+
+
+    }
